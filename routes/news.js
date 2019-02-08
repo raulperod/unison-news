39d97bb2eeda883ecd2c3ede25adf518c94df6a9
Raul Perez <raulperod@gmail.com>
2019-02-07 22:54:33 -0700

    edit and delete news

--- routes/news.js
+++ routes/news.js
@@ -1,7 +1,9 @@
 const   router = require('express').Router(),
         News = require('../models/news'),
         Department = require('../models/department'),
-        Image = require('../models/image')
+        Image = require('../models/image'),
+        { unlink } = require('fs-extra'),
+        path = require('path'),
         session_active = require('../middlewares/session_active'),
         moment = require('moment')
 
@@ -65,6 +67,81 @@ router.get('/list', session_active, async (req, res) => {
         url = user.type > 0 ? 'news/list_admin' : 'news/list_author'
 
     res.render(url, {user, news: await News.find({}).populate('department author') })
+})
+
+router.get('/edit/:idNews', session_active, async (req, res) => {
+    let { idNews } = req.params,
+        { user } = req.session,
+        url = user.type > 0 ? 'news/edit_admin' : 'news/edit_author'
+
+    let news = await News.findById(idNews).populate('author department')
+
+    res.render(url, {user, news, departments: await Department.find({}), messages: {type:0, message:""} })
+})
+
+router.post('/edit/:idNews', session_active, async (req, res) => {
+    let { user } = req.session,
+        { idNews } = req.params,
+        { title } = req.body,
+        { department_name } = req.body,
+        { body } = req.body,
+        { url } = req.body,
+        { start_date } = req.body,
+        { finish_date } = req.body,
+        urlv = user.type > 0 ? 'news/edit_admin' : 'news/edit_author'
+        
+    // string to date
+    start_date = moment(start_date).toDate()
+    finish_date = moment(finish_date).toDate()
+
+    if(finish_date < start_date){
+        res.render(urlv, {
+            user, 
+            departments: await Department.find({}), 
+            messages: {type:1, message:"La fecha de cierre es antes que la de inicio.", title, department_name, body, url, start_date, finish_date} 
+        })
+    }
+
+    let department = await Department.findOne({name:department_name})
+
+    if(!department){
+        res.redirect('/d/new')
+    }
+
+    let updateNews = await News.findById(idNews).populate('image')
+    updateNews.title = title
+    updateNews.department = department
+    updateNews.author = req.session.user
+    updateNews.body = body
+    updateNews.url= url
+    // delete and update image
+    if (req.file !== undefined){
+        let updateImage = updateNews.image
+        await unlink(path.resolve('./public' + updateImage.path))
+        updateImage.filename = req.file.filename,
+        updateImage.path = '/uploads/' + req.file.filename
+        updateImage.originalname = req.file.originalname
+        updateImage.mimetype = req.file.mimetype
+        updateImage.size = req.file.size
+        updateImage.save()
+    }
+    
+    updateNews.start_date = start_date
+    updateNews.finish_date = finish_date
+
+    await updateNews.save()
+
+    res.redirect('/n/list')
+})
+
+router.get('/delete/:idNews', session_active, async (req, res) => {
+    let { idNews } = req.params
+
+    let deleteNews = await News.findById(idNews).populate('image')
+    await unlink(path.resolve('./public' + deleteNews.image.path))
+    await Image.findByIdAndDelete(deleteNews.image._id)
+    await News.findByIdAndDelete(idNews)
+    res.redirect('/n/list')
 })
 
 router.get('/show/:department_name', session_active, async (req, res) => {
