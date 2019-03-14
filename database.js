1c80831433933bcea322ba0b70099ec05ba222bd
Raul Perez <raulperod@gmail.com>
2019-03-14 13:12:48 -0700

    env

--- database.js
+++ database.js
@@ -1,6 +1,5 @@
 const   mongoose = require('mongoose'),
-        //URI = 'mongodb://localhost:27017/unison-news'
-        URI = "mongodb://unison-newsuser:unis0n-n3wspa66w0rd@ds231658.mlab.com:31658/unison-news"
+        URI = process.env.URI || 'mongodb://localhost:27017/unison-news'
 
 mongoose.connect(URI, { useNewUrlParser: true })
     .then(DB => console.log('DB is conected'))
